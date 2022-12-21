093d3fba79b47ac8822be1134f042d950e6a7bf6
Dave Kanter <dave.kanter@publicis.com>
2022-12-20 17:54:26 -0800

    Finished!  And the answer is correct.
    Wanted to capture some of the things I learned.
    
    *   Test EVERYTHING.  I spent a lot of time looking for errors in the wrong places.
    *   No problem can be solved by throwing more code at it.  The excursion into a class was a huge waste of time (see last item).
    *   Python is FUNCTIONAL.  Don't turn everything into a variable.
    *   Maybe consider using some of the newer Python libraries for some of this stuff.

--- day05.py
+++ day05.py
@@ -1,78 +1,24 @@
 # crates are moved one at a time
 # get a pushing and a popping
 
-# regex time!
+# define regex to pull every one or two digit number out of the line
 import re
-
-# pull every one or two digit number out of the line
-moveregex = re.compile('\d{1,2}')
-
-# class line_machine:
-#     """ class docstring """
-
-    # def __init__(self):
-    #     self.containers = [[] for i in range(9)]          # Instance Variable
-    #     self.moves = []
-
-
-    # def lineSplitter(self, line):
-    #     """convert initial container state into lists"""
-    #     for base in range(0, 9):
-    #         left = base * 4
-    #         right = left + 3
-    #         char = line[left+1:right-1]
-    #         if char == ' ':
-    #             continue
-    #         else:    
-    #             self.containers[base].append(char)
-    #         # print(containers)
-
-    # def make_move(self, amt, start, end):
-    #     """move the containers around"""
-    #     cargo = []
-    #     start -= 1
-    #     end -= 1
-    #     for x in range(amt):
-    #         # print(amt)
-    #     #for amt in range:
-    #         cargo = self.containers[start][:amt]
-    #         cargo = cargo.reverse()
-    #         self.containers[end].insert(0, cargo)
-    #         del self.containers[start][:amt]
-
-    # def move_maker(self):
-    #     """ make all of the moves """
-    #     local_containers = self.containers.copy()
-    #     # print(f'Containers: {self.containers}')
-        # print(f'local_containers: {local_containers}')
-        # print(f'self.moves = {self.moves}')
-        # print(f'type: {type(local_containers)}')
-        
-        # for move in self.moves:
-        #     amt = int(move[0])
-        #     start = int(move[1]) - 1
-        #     end = int(move[2]) - 1
-        #     for x in range(amt):
-        #         cargo = local_containers[start][:amt]
-        #         cargo = cargo.reverse()
-        #         local_containers[end].insert(0, cargo)
-        #         del local_containers[start][:amt]
-
-        # return local_containers
-    
+move_regex = re.compile('\d{1,2}')
+ 
 def main():
-    """ Ties it all together """
+    """ Advent of code day 5 """
 
-
+    # set up the data
     containers = [[] for i in range(9)]          # Instance Variable
     moves = []
 
-    with open('day05_data.txt') as file:
+    # read everything and split into containers and moves
+    with open('day05_data.txt', encoding='UTF-8') as file:
         for line in file:
             # container splitter
             if line[0:4] == 'move':
                 # move splitter
-                moves.append(moveregex.findall(line))
+                moves.append(move_regex.findall(line))
             else:
                 # print("open:", containers)
                 for base in range(0, 9):
@@ -82,23 +28,22 @@ def main():
                     if char != ' ':
                         containers[base].append(char)
 
+    # apply the moves to the containers
     for move in moves:
         amt = int(move[0])
         start = int(move[1]) - 1
         end = int(move[2]) - 1
-        for x in range(amt):
-            cargo = containers[start][:amt]
-            cargo = cargo.reverse()
-            containers[end].insert(0, cargo)
-            del containers[start][:amt]
-    # print(f'MOVES: {moves}')
+        cargo = containers[start][:amt]
+        cargo.reverse()
+        containers[end][0:0] = cargo
+        del containers[start][:amt]
     print(f'CONTAINERS: {containers}')
-    # self.moveMaker()      
 
-    # print(f'{self.move_maker()}')
-    # print(self.containers)
-    #  return self.containers
+    # print it
+    outstr = ''
+    for item in containers:
+        outstr += item[0]
 
-
+    print(outstr)
 
 main()
